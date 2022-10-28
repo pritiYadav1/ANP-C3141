4e5715fe2a23d407e5c28d0d341b90e9092f7a1c
pritiYadav1 <115352601+pritiYadav1@users.noreply.github.com>
2022-10-28 15:21:21 +0530

    Update First_pro.java

--- First_pro.java
+++ First_pro.java
@@ -7,6 +7,7 @@ public class First_pro
 		System.out.print("Hiii this byu using Sysout :)");
 		System.out.print("\nThis si second \t line :-");
 		System.out.println("\n\"PRITI\"");
+		System("This is my new line");
 	}
 
 }
